29fe588cbf261dc07c3f013f6abdf79d04fbaa61
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2023-11-29 15:39:12 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-blue-400 text-2xl text-center'>Kaling Store</h1>
+
+    </main>
+  )
+}
