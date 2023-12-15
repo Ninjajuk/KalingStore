a58a719196c592482484b0fe3897fa0e3b8c13c4
Samsu <biosamsuddin@gmail.com>
2023-12-15 16:47:05 +0530

    veg and fruits slider

--- app/Product/FriutsVegetables.js
+++ app/Product/FriutsVegetables.js
@@ -1,104 +1,114 @@
+'use client'
+
 import { FaAngleLeft,FaAngleRight } from "react-icons/fa";
-import React from 'react';
+import React, { useState } from 'react';
 import CategoryCard from "./components/CardFruitsVeg";
 
-
-// const CategoryCard = ({
-//   title,
-//   description,
-//   thumbnail,
-//   price,
-//   TotalPrice,
-//   handleAddToCart
-// }) => {
-//   const weightOptions = [1, 2, 3, 5]; // You can modify the weight options as needed
-
-//   return (
-//     <div className="bg-white p-6 rounded-lg shadow-md mb-4 min-w-[14rem] flex flex-col ">
-//       <div className="w-full h-2/3">
-//         <img
-//           src={thumbnail}
-//           alt={title}
-//           className=" rounded-md w-full h-full object-cover"
-//         />
-//       </div>
-
-//       <div>
-//         <p className="text-md text-slate-800 font-semibold ">Fresh</p>
-//         <h2 className="text-xl font-semibold mb-2">{title}</h2>
-//         <div className="py-2">
-//           {/* <label htmlFor={`${title}-weight`} className="text-gray-700">
-//             Select Weight:
-//           </label> */}
-//           <select
-//             id={`${title}-weight`}
-//             className="block w-full p-2 border border-gray-300 rounded-md mt-1"
-//           >
-//             {weightOptions.map((weight, index) => (
-//               <option key={index} value={weight}>
-//                 {weight} kg
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//         <p className="text-gray-700 flex items-center">
-//           <span className="text-lg font-bold">₹{price}<sub>/kg</sub></span>
-//           <span className="ml-2 line-through">₹{TotalPrice}</span>
-//         </p>
-//       </div>
-
-//       <div className="w-full flex items-center justify-center ring-1 rounded-md ring-red-500 my-2 hover:bg-red-500 hover:text-white cursor-pointer">
-//         <button onClick={()=>handleAddToCart({  title,
-//   description,
-//   thumbnail,
-//   SellingPrice,
-//   TotalPrice,})} type="button" className="px-4 py-2 ">
-//           Add
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-
+ const categories = [
+  {
+    id: 101,
+    title: "Carrots",
+    description: "Explore the latest additions to our collection.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/l/10000070_15-fresho-carrot-orange.jpg?tr=w-640,q=80",
+    price: 25,
+    TotalPrice: 35,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 102,
+    title: "Capsicum",
+    description: "Boost your productivity with these essentials.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/10000067_23-fresho-capsicum-green.jpg?tr=w-1920,q=80",
+    price: 36,
+    TotalPrice: 50,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 103,
+    title: "Tomato",
+    description: "Create your ideal workspace with our curated selection.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/10000203_16-fresho-tomato-local.jpg?tr=w-3840,q=80",
+    price: 45,
+    TotalPrice: 70,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 104,
+    title: "Potato",
+    description: "Fresho Potato",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/l/40048459_8-fresho-potato-new-crop.jpg?tr=w-640,q=80",
+    price: 15,
+    TotalPrice: 35,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 105,
+    title: "Onion",
+    description: "Add a flavorful twist to your dishes with our premium onions.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/1201414_1-fresho-onion.jpg?tr=w-2048,q=80",
+    price: 20,
+    TotalPrice: 30,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 106,
+    title: "Cucumber",
+    description: "Stay cool and refreshed with our crisp cucumbers.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/40077518_1-fresho-cucumber.jpg?tr=w-2048,q=80",
+    price: 18,
+    TotalPrice: 25,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 107,
+    title: "Cauliflower",
+    description: "Enhance your meals with our fresh and tender cauliflower.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/10000074_19-fresho-cauliflower.jpg?tr=w-2048,q=80",
+    price: 30,
+    TotalPrice: 45,
+    brand: "yinkiong",
+    category: "vegetables"
+  },
+  {
+    id: 108,
+    title: "Lady's Finger",
+    description: "Experience the goodness of lady's finger in every bite.",
+    thumbnail: "https://www.bigbasket.com/media/uploads/p/m/10000142_17-fresho-ladies-finger.jpg?tr=w-2048,q=80",
+    price: 22,
+    TotalPrice: 32,
+    brand: "yinkiong",
+    category: "vegetables"
+  }
+];
 
 const FruitsVegetables = () => {
 
+  const [currentindex,setCurrentindex]=useState(0)
+  const itemsPerpage=4
 
+ // Change from 'data.length' to 'categories.length'
+const handleNext = () => {
+  const newIndex = currentindex + itemsPerpage;
+  if (newIndex < categories.length) {
+    setCurrentindex(newIndex);
+  }
+};
 
+const handlePrev = () => {
+  const newIndex = currentindex - itemsPerpage;
+  if (newIndex >= 0) {
+    setCurrentindex(newIndex);
+  }
+};
 
-  const categories = [
-    {
-      title: 'Carrots',
-      description: 'Explore the latest additions to our collection.',
-      thumbnail: 'https://www.bigbasket.com/media/uploads/p/l/10000070_15-fresho-carrot-orange.jpg?tr=w-640,q=80',
-      SellingPrice:25,
-      TotalPrice:35
-    },
-    {
-      title: 'Capsicum',
-      description: 'Boost your productivity with these essentials.',
-      thumbnail: 'https://www.bigbasket.com/media/uploads/p/m/10000067_23-fresho-capsicum-green.jpg?tr=w-1920,q=80',
-      SellingPrice:36,
-      TotalPrice:50
-    },
-    {
-      title: 'Tomato',
-      description: 'Create your ideal workspace with our curated selection.',
-      thumbnail: 'https://www.bigbasket.com/media/uploads/p/m/10000203_16-fresho-tomato-local.jpg?tr=w-3840,q=80',
-      SellingPrice:45,
-      TotalPrice:70
-    },
-    {
-      title: 'Potato',
-      description: 'Fresho Potato',
-      thumbnail: 'https://www.bigbasket.com/media/uploads/p/l/40048459_8-fresho-potato-new-crop.jpg?tr=w-640,q=80',
-      SellingPrice:15,
-      TotalPrice:35
-    //   image: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-01.jpg',
-    },
-  ];
-  const weightOptions = [1, 2, 3, 5];
+  const visibleCards = categories.slice(currentindex, currentindex + itemsPerpage);
+
 
   return (
     <section className="w-full bg-gray-100">
@@ -110,8 +120,8 @@ const FruitsVegetables = () => {
           </h1>
           <div className='flex items-center space-x-4'>
             <a href='' className="hover:underline text-slate-900 font-semibold text-lg">View All</a>
-            <a href='' className="hover:rounded-full hover:bg-gray-400"><FaAngleLeft className="w-6 h-6"/></a>
-            <a href='' className="hover:rounded-full hover:bg-gray-400"><FaAngleRight className="w-6 h-6"/></a>
+            <button onClick={handlePrev} className="cursor-pointer hover:rounded-full hover:bg-gray-400" disabled={currentindex === 0}><FaAngleLeft className="w-6 h-6"/></button>
+            <button onClick={handleNext} className="cursor-pointer hover:rounded-full hover:bg-gray-400"disabled={currentindex + itemsPerpage >= categories.length}><FaAngleRight className="w-6 h-6"/></button>
 
           </div>
         </div>
@@ -120,7 +130,7 @@ const FruitsVegetables = () => {
           {/* {categories.map((category, index) => (
             <CategoryCard key={index} {...category} />
           ))} */}
-          <CategoryCard/>
+          <CategoryCard visibleCards={visibleCards}/>
         </div>
       </div>
     </section>
